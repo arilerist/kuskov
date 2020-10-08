8d5e03f12a7cbcea2c2acd8538784b2602482475
arilerist <72568819+arilerist@users.noreply.github.com>
2020-10-08 23:18:22 +0700

    Some changes

---
+++ qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5f7f3a1741c25f001ec3c71e",
+				"5f7f3a1741c25f001ec3c720"
+			]
+		},
+		"5f7f3a1741c25f001ec3c71e": {
+			"id": "5f7f3a1741c25f001ec3c71e",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5f7f3a1741c25f001ec3c720": {
+			"id": "5f7f3a1741c25f001ec3c720",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
